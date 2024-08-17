06dae6d5769f25f7fab7715b7c805c536be7633a
GECBVN_220213107012_Pooja Singh_CE <singhpooja2944@gmail.com>
2024-08-17 11:58:54 +0530

    Learned Redux: Action, Reducer and Store.

--- React Redux/index.js
+++ React Redux/index.js
@@ -2,6 +2,7 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 //* 1. An action is a object that describes what happened.
 // It has type property that has string constants. Name of action
@@ -16,9 +17,15 @@ const buyCake = () => {
   };
 };
 
-//* 2. state is a single object that represents teh sate
+const buyIceCream = () => {
+  return {
+    type: BUY_ICECREAM,
+  };
+};
+//* 2. state is a single object that represents the sate
 const initialState = {
   numberOfCakes: 10,
+  numberOfIceCreams: 20,
 };
 
 //* 3. Reducers are pure function that takes initialState and action as parameter and returns the new state as object.
@@ -27,6 +34,8 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return { ...state, numberOfCakes: state.numberOfCakes - 1 };
+    case BUY_ICECREAM:
+      return { ...state, numberOfIceCreams: state.numberOfIceCreams - 1 };
     default:
       return state;
   }
@@ -35,14 +44,17 @@ const reducer = (state = initialState, action) => {
 //* 4. Redux store: It store the state of the entire application
 const store = createStore(reducer);
 
-console.log("Initial state:", store.dispatch());
+console.log("Initial state:", store.getState());
 
 const unSubscribe = store.subscribe(() =>
   console.log("Updated State: ", store.getState())
 );
-
+//* Only write dispatch(actionCreator) after subscribing the application with the store
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
 unSubscribe();

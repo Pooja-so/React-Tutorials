7127e38673688af5f49cb2b7b13b7effed47a583
GECBVN_220213107012_Pooja Singh_CE <singhpooja2944@gmail.com>
2024-08-20 19:17:43 +0530

    Applied middleware in the redux store for logging the state change in console on action button clicked.

--- Redux Series/react-redux-demo/src/redux/store.js
+++ Redux Series/react-redux-demo/src/redux/store.js
@@ -1,6 +1,11 @@
 import { createStore } from "redux";
+
+// Import 2 things for applying middleware
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
+
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
